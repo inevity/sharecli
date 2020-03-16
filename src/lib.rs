ef5041c30fd54be6d340124ccaac2f201f8defa5
Baul <roidinev@gmail.com>
2020-03-16 09:49:50 +0800

    cmd parse done

---
+++ src/lib.rs
@@ -0,0 +1,27 @@
+//pub fn hello() {
+//    println!("Hello, world!");
+//}
+
+// pub fn hello() -> String {
+//   //! This returns `Hello, world!` String
+//   ("Hello, world!").to_string()
+// }
+// 
+// // 01. Tests for `hello()`
+// #[test] // Indicates that this is a test function
+// fn test_hello() {
+//   assert_eq!(hello(), "Hello, world!");
+// }
+// 
+// // 02. Tests for `hello()`, Idiomatic way
+// #[cfg(test)] // Only compiles when running tests
+// mod tests { // Separates tests from code
+//   use super::hello; // Import root `hello()` function
+// 
+//     #[test]
+//     fn test_hello() {
+//         assert_eq!(hello(), "Hello, world!");
+//     }
+// }
+
+
